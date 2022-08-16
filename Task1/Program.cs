de071d373e9003ca7225ad5423ad26265ff55ecc
maximandreev <maxands@yandex.ru>
2022-08-16 14:31:34 +0300

    Task 1 completed

---
+++ Task1/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Для окончания работы введите \"стоп\"");
+int A = 0;
+int B = 0;
+string input = "";
+double result = 0;
+while (true)
+{
+    Console.WriteLine("Введите число А:");
+    input = Console.ReadLine().ToLower();
+    if (input == "стоп")
+    {
+        break;
+    }
+    A = Convert.ToInt32(input);
+    Console.WriteLine("Введите число B:");
+    B = Convert.ToInt32(Console.ReadLine());
+    result = Math.Pow(A, B);
+    Console.WriteLine($"Число А в степени В равно: {result}");
+}
