34c2448c7458cfeedd259d5ce44a9565f9f058df
maximandreev <maxands@yandex.ru>
2022-08-16 14:42:07 +0300

    Task 3 completed

---
+++ Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿void FillArray(int[] array)
+{
+    int x = 0;
+    for (int i=0; i<array.Length; i++)
+    {
+        x = new Random().Next(1, 100);
+        array[i] = x;
+    }
+}
+
+
+void PrintArray(int[] array)
+{
+    foreach (int element in array)
+    {
+        Console.Write(element+" ");
+    }
+}
+
+
+Console.WriteLine("Введите длину массива:");
+int length = Convert.ToInt32(Console.ReadLine());
+int[] array = new int[length];
+FillArray(array);
+PrintArray(array);
